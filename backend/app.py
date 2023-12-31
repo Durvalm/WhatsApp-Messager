21d4edd29fdde2983b6a3ff56a9ce5c7a00755e0
Durvz Maia <89949017+Durvalm@users.noreply.github.com>
2023-12-31 00:03:19 -0500

    test git ignore

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,16 @@
 from flask import Flask
+from settings import db_session, init_db
+from models import User
 
 app = Flask(__name__)
 
+init_db()
+
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    db_session.remove()
+
 @app.route("/")
 def hello_world():
+    print(User.query.all())
     return "<p>Hello World</p>"
---
+++ backend/models.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String
+from settings import Base
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), unique=True)
+
+    def __init__(self, name=None, email=None):
+        self.name = name
+
+    def __repr__(self):
+        return f'<User {self.name!r}>'
