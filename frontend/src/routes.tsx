7685dbe3213db3d5d3ee4be15dde71bf0663a3d5
Durvz Maia <89949017+Durvalm@users.noreply.github.com>
2024-01-03 10:54:52 -0500

    redirect to login if not auth

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,12 +1,29 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate, useNavigate } from 'react-router-dom'
 import { Signup } from './pages/auth/Signup'
 import { Login } from './pages/auth/Login'
 import { Whatsapp } from './pages/Whatsapp'
+import { useContext, useEffect } from 'react'
+import { AuthContext } from './contexts/AuthContext'
 
 export function Router() {
+  const navigate = useNavigate()
+
+  const { authenticatedUser } = useContext(AuthContext)
+
+  useEffect(() => {
+    if (!authenticatedUser) {
+      // Redirect to login
+      navigate('/login', { replace: true })
+    }
+  }, [authenticatedUser, navigate])
   return (
     <Routes>
-      <Route path="/" element={<Whatsapp />} />
+      <Route
+        path="/"
+        element={
+          authenticatedUser ? <Whatsapp /> : <Navigate to="/login" replace />
+        }
+      />
       <Route path="/signup" element={<Signup />} />
       <Route path="/login" element={<Login />} />
     </Routes>
